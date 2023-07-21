0995edab3c0da4fbb9cd5068722909426329ffa7
ayush.a <ayush.a@L-DEV-0171>
2023-07-21 23:00:11 +0530

    two way data binding using ngModel

--- AngIntro/src/app/app.module.ts
+++ AngIntro/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
@@ -16,6 +16,7 @@ import { PostListComponent } from './post-list/post-list.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- AngIntro/src/app/post-list/post-list.component.ts
+++ AngIntro/src/app/post-list/post-list.component.ts
@@ -8,6 +8,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class PostListComponent {
   @Input() fromParent:string='';
   message:string='postlist-to-post';
+  inputValue:string='';
   @Output() messageEvent=new EventEmitter<string>();
   flag:boolean=false;
   imgUrl:string='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80';
@@ -20,9 +21,8 @@ export class PostListComponent {
     console.log("bamabzoo")
   }
 
-  onKeyUp(inputValue:string){
-    console.log(inputValue);
-    
+  onKeyUp(){
+    console.log(this.inputValue);
     
   }
 }
