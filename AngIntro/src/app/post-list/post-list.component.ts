7e416405b9cce9e6f2cc577312ac6df5042a865a
ayush.a <ayush.a@L-DEV-0171>
2023-07-21 19:56:34 +0530

    property binding,style binding, class binding

--- AngIntro/src/app/post-list/post-list.component.ts
+++ AngIntro/src/app/post-list/post-list.component.ts
@@ -9,7 +9,8 @@ export class PostListComponent {
   @Input() fromParent:string='';
   message:string='postlist-to-post';
   @Output() messageEvent=new EventEmitter<string>();
-
+  flag:boolean=false;
+  imgUrl:string='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80';
   sendMessage(){
     console.log('emitted')
     this.messageEvent.emit(this.message);
